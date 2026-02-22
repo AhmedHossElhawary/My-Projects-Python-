4f230ecd139fcc79437b4d5b2fea2913aeaf598f
Ahmed Hossam <apolo.hoss@gmail.com>
2026-02-22 23:11:52 +0200

    commit wrong-name

--- wrong-name.py
+++ wrong-name.py
@@ -1,7 +1,10 @@
 me = input("Who is the most handsome man in the world? \n")
-if me.lower() == "ahmed" or me.lower() == "ahmed hossam" or me.lower() == "hossam" or me.lower() == "hossam hassan" or me.lower() == "ahmed hossam hassan" or me.lower() == "you":
-    print("Habibi")
-elif me.lower() == "the prophet mohammed" or me.lower() == "prophet mohammed" or me.lower() == "pbuh":
-    print("Respect")
-else:
-    print("You are wrong :(") 
+while True:
+    if me.lower() == "ahmed" or me.lower() == "ahmed hossam" or me.lower() == "hossam" or me.lower() == "hossam hassan" or me.lower() == "ahmed hossam hassan" or me.lower() == "you":
+        print("Habibi")
+        break
+    elif me.lower() == "the prophet mohammed" or me.lower() == "prophet mohammed" or me.lower() == "pbuh":
+        print("Respect")
+        break
+    else:
+        me = input("You are wrong :(. Try again: \n")
